c34738b6ad242df84f2edd8b321a13fcb978e3b0
_tireDep <wing997@naver.com>
2020-08-28 00:54:08 +0900

    [벽 판정 및 일반/호밍 미사일 테스트]
    - 테스트총알을 발사해 벽과의 충돌 유무로 벽 판정 테스트
    
    - 일반 미사일 / 호밍(유도) 미사일 테스트
    -> 수정 필요

--- FocusGame/FocusGame/CannonClass.cpp
+++ FocusGame/FocusGame/CannonClass.cpp
@@ -30,7 +30,13 @@ Cannon::Cannon(POINT set)
 	timer = tmTime->tm_sec;
 	countDownSec = dCountdown;
 
-	testShot = hitRect;
+	testShot.left = set.x - eBlockSize * 0.5;
+	testShot.top = set.y - eBlockSize * 0.5;
+	testShot.right = set.x + eBlockSize * 0.5;
+	testShot.bottom = set.y + eBlockSize * 0.5;
+
+	tempSpot.x = -1;
+	tempSpot.y = -1;
 }
 
 Cannon::~Cannon()
@@ -47,6 +53,8 @@ void Cannon::DrawObject(HDC hdc)
 {
 	SelectObject(hdc, GetStockObject(NULL_BRUSH));
 	Rectangle(hdc, hitRect.left, hitRect.top, hitRect.right, hitRect.bottom);	// test
+
+	Rectangle(hdc, testShot.left, testShot.top, testShot.right, testShot.bottom);	// test_testShot
 }
 
 void Cannon::CheckInPlayer()
@@ -55,27 +63,162 @@ void Cannon::CheckInPlayer()
 	vector<TileMap> mapPos = dGameManager->GetNowMap();
 	RECT playerPos = dGameManager->GetNowPlayerPos();
 
+	bool isCanShoot = false;
 	if (IntersectRect(&area, &hitRect, &playerPos))
 	{
 		// todo : 벽 판정 필요 -> 테스트 총알 발사로 확인?
-		
-		// >> countdownTimer
-		time(&nowTime);
-		tmTime = localtime(&nowTime);
+		POINT addSpot;
+		POINT playerCenter;
+		playerCenter.x = (playerPos.left + playerPos.right) * 0.5;
+		playerCenter.y = (playerPos.top + playerPos.bottom) * 0.5;
 
-		if (timer != tmTime->tm_sec)
+		// if (tempSpot.x == -1 && tempSpot.y == -1)
+		// 	tempSpot = centerPos;
+		// 
+		// float calc = sqrt(float(playerCenter.x - tempSpot.x) * float(playerCenter.x - tempSpot.x) + float(playerCenter.y - tempSpot.y)*float(playerCenter.y - tempSpot.y));
+		// 
+		// if (calc)
+		// {
+		// 	addSpot.x = (playerCenter.x - tempSpot.x) / calc * 5;
+		// 	addSpot.y = (playerCenter.y - tempSpot.y) / calc * 5;
+		// }
+		// else
+		// {
+		// 	addSpot.x = 0;
+		// 	addSpot.y = 5;
+		// }
+
+		// tempSpot.x += addSpot.x;
+		// tempSpot.y += addSpot.y;
+
+		static POINT tempPlayer;	// 임시
+		if (tempSpot.x == -1 && tempSpot.y == -1)
 		{
-			timer = tmTime->tm_sec;
-			countDownSec--;
-			printf("testTime\n");
+			tempSpot = centerPos;
+			tempPlayer = playerCenter;
 		}
 
-		if (countDownSec == 0)
+		if (playerCenter.x > tempSpot.x) // (tempPlayer.x > tempSpot.x)
+			tempSpot.x += 5;
+		else if (playerCenter.x < tempSpot.x) // (tempPlayer.x < tempSpot.x)
+			tempSpot.x -= 5;
+		//else
+		//	tempSpot.x = 0;
+
+		if (playerCenter.y > tempSpot.y) // (tempPlayer.y > tempSpot.y)
+			tempSpot.y += 5;
+		else if (playerCenter.y < tempSpot.y) // (tempPlayer.y < tempSpot.y)
+			tempSpot.y -= 5;
+		//else
+		//	tempSpot.y = 0;
+
+		testShot.left = tempSpot.x - eBlockSize * 0.5;
+		testShot.top = tempSpot.y - eBlockSize * 0.5;
+		testShot.right = tempSpot.x + eBlockSize * 0.5;
+		testShot.bottom = tempSpot.y + eBlockSize * 0.5;
+
+		for (int i = 0; i < mapPos.size(); i++)
 		{
-			countDownSec = dCountdown;
-			printf("카운트 타임 오버\n");
+			if (IntersectRect(&area, &mapPos[i].pos, &testShot) && mapPos[i].type == eMapBlock)
+			{
+				printf("hit");
+				testShot.left = centerPos.x - eBlockSize * 0.5;
+				testShot.top = centerPos.y - eBlockSize * 0.5;
+				testShot.right = centerPos.x + eBlockSize * 0.5;
+				testShot.bottom = centerPos.y + eBlockSize * 0.5;
+
+				tempSpot.x = -1;
+				tempSpot.y = -1;
+
+
+				break;
+			}
+
+			if (IntersectRect(&area, &playerPos, &testShot))
+			{
+				printf("playerHit");
+				isCanShoot = true;
+
+				testShot.left = centerPos.x - eBlockSize * 0.5;
+				testShot.top = centerPos.y - eBlockSize * 0.5;
+				testShot.right = centerPos.x + eBlockSize * 0.5;
+				testShot.bottom = centerPos.y + eBlockSize * 0.5;
+
+				tempSpot.x = -1;
+				tempSpot.y = -1;
+
+				break;
+			}
+			
+			// if()
+		}
+
+
+		// >> countdownTimer
+		if (isCanShoot)
+		{
+			time(&nowTime);
+			tmTime = localtime(&nowTime);
+
+			if (timer != tmTime->tm_sec)
+			{
+				timer = tmTime->tm_sec;
+				countDownSec--;
+				printf("testTime\n");
+			}
+
+			if (countDownSec == 0)
+			{
+				countDownSec = dCountdown;
+				printf("카운트 타임 오버\n");
+			}
 		}
 		// >> countdownTimer
+
+		// >>testShot
+	
+		// printf("%d %d\n", playerCenter.x, playerCenter.y);
+		// int arc = (int) (-atan2(abs(centerPos.y - playerCenter.y), abs(centerPos.y - playerCenter.y)) * 180) / 3.14 + 90;
+		// int largex = 1, largey = -1;
+		// 
+		// // if (playerCenter.y - centerPos.y > 0)
+		// // {
+		// // 	largey = 1;
+		// // 	if (playerCenter.x - centerPos.y > 0)
+		// // 	{
+		// // 		largex = -1;
+		// // 		arc = arc + 90;
+		// // 	}
+		// // 	else
+		// // 		arc = 270 - arc;
+		// // }
+		// // else
+		// // {
+		// // 	if (playerCenter.x - centerPos.x > 0)
+		// // 	{
+		// // 		largey = -1;
+		// // 		arc = 90 - arc;
+		// // 	}
+		// // 	else
+		// // 		arc = 270 + arc;
+		// // }
+		// 
+		// // POINT homingSpot;
+		// // homingSpot.x
+		// // centerPos.x += sin()
+		// double sinArr[360];
+		// double cosArr[360];
+		// 
+		// for (int i = 0; i < 360; i++)
+		// {
+		// 	sinArr[i] = sin(i*0.0175);
+		// 	cosArr[i] = cos(i*0.0175);
+		// }
+		// 
+		// centerPos.x += sinArr[arc] * 5;
+		// centerPos.y -= cosArr[arc] * 5;
+
+		// >>testShot
 	}
 	return;
 }
